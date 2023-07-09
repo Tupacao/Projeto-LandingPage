2ba2381c6c21e3469f70967a19b790da6b1e0bc8
Arthur_Santos <arthur12268@gmail.com>
2023-07-09 20:58:24 -0300

    animacoes

--- java.js
+++ java.js
@@ -10,3 +10,23 @@ function Abrir (){
         contador = 0;
     }
 }
+
+function Animacao_1 () {
+    const esquerda = document.querySelector('[data-animate = left]')
+    esquerda.classList.add('animate')
+}
+
+function Animacao_2 () {
+    const esquerda = document.querySelector('[data-animate = rigth]')
+    esquerda.classList.add('animate')
+}
+
+function Animacao_3 () {
+    const esquerda = document.querySelector('.servicos')
+    esquerda.classList.add('animate')
+}
+
+function Animacao_4 () {
+    const esquerda = document.querySelector('.diferencial')
+    esquerda.classList.add('animate')
+}
